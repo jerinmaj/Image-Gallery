aa38431126a0134db8d3e727a4e68656712342f5
jerin <jerinmajeed95@gmail.com>
2019-06-03 20:38:34 -0700

    first commit

--- image-Gallery/src/app/app.module.ts
+++ image-Gallery/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms'
+// import { AngularFireModule} from '@angular/fire';
+// import { AngularFireStorageModule} from '@angular/fire/storage';
+// import { AngularFireDatabaseModule} from '@angular/fire/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImageComponent } from './image/image.component';
+import { ImagesComponent } from './images/images.component';
+import { ImageListComponent } from './images/image-list/image-list.component';
+import { ImageComponent } from './images/image/image.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ImagesComponent,
+    ImageListComponent,
     ImageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+    // AngularFireModule,
+    // AngularFireStorageModule,
+    // AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- image-Gallery/src/app/images/image/image.component.ts
+++ image-Gallery/src/app/images/image/image.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-image',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageComponent implements OnInit {
 
+
+  formTemplate = new FormGroup({
+    caption :new FormControl(''),
+    category :new FormControl(''),
+    imageUrl :new FormControl(''),
+  })
   constructor() { }
 
   ngOnInit() {
